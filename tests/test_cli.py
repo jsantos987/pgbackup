c3b406967b5d69afd63fa298266eb5777a305ba2
jsantos987 <jas.santos.987@gmail.com>
2018-02-28 20:46:03 +0000

    code is a passing when it should and failing when it should

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -30,3 +30,29 @@ def test_parser_with_driver_and_destination():
 
 	assert args.driver == 'local'
 	assert args.destination == '/some/path'
+
+
+def test_parser_with_unknown_driver():
+	"""
+	This one will exit if the driver name is unknown.
+	"""
+	parser = cli.create_parser()
+ 	with pytest.raises(SystemExit):
+ 		parser.parse_args([url, '--driver', 'azure', 'destination'])
+
+
+def test_parser_with_known_drivers():
+	"""
+	The parser will not exit if the driver name is known.
+	"""
+	parser = cli.create_parser()
+	
+	for driver in ['local', 's3']:
+		assert parser.parse_args([url, '--driver', driver, 'destination'])
+
+#	with pytest.raises(SystemExit):
+#		parser.parse_args([url, '--driver', 'azure', 'destination'])
+
+
+
+	
